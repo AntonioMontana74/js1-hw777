738b47f1e0315cd835a0041371b903da905c722d
AntonioMontana74 <149238712+AntonioMontana74@users.noreply.github.com>
2023-11-26 10:01:26 +0300

    Add files via upload

---
+++ hw-7/js/main.js
@@ -0,0 +1,40 @@
+class User {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    
+    
+    print() {
+        console.log(`Имя - ${this.name}; Возраст - ${this.age}`);
+    }
+}
+let user = new User("Нурислам", 15);
+
+
+
+class Student extends User {
+    #password;
+    
+    constructor(name, age, username, password) {
+        super(name, age);
+        this.username = username;
+        this.#password = password;
+    }
+    
+    print(Password) {
+        if (Password === this.#password) {
+            console.log(`Имя - ${this.name}; Возраст - ${this.age}; Имя пользователя - ${this.username}`);
+
+    } else {
+      console.log("Имя пользователя или пароль неверны");
+    }
+}
+}
+
+
+let student = new Student("Нурислам", 15, "Pashtet", "nur1212");
+
+student.print("nur1212");
+
+
